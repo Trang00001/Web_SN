3699e091c2ec206bc043e9a7a3080e028102a878
Trang00001 <lemaitrang000@gmail.com>
2025-09-29 17:13:05 +0700

    add database

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 // Cấu hình cơ sở dữ liệu
 define("HOST", "localhost");
-define("DB", "db_kinhdoanh");
+define("DB", "social_network");
 define("USER", "root");
 define("PASSWORD", "");
 
-// Base URL dự án
-define("BASE_URL", "http://localhost/project/");
+// Base URL của dự án (chỉnh lại cho đúng tên folder bạn đặt trong htdocs)
+define("BASE_URL", "http://localhost/SN_WEB/");
 
-// Các hằng số khác có thể thêm ở đây
-?>
+// Thiết lập charset mặc định
+define("DB_CHARSET", "utf8mb4");
